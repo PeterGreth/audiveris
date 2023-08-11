135862b6ea1419544e9b2abe8c5c60cfc7b30bac
Peter Greth <peter.greth@gmx.de>
2023-08-11 21:56:46 +0200

    Simplify workflow and fix unit test

--- .github/install-tessdata.sh
+++ .github/install-tessdata.sh
@@ -1,30 +1,39 @@
 #!/bin/sh
 set -e
 
-verify_arguments_given() {
+verify_single_argument_given() {
   if [ $# -eq 0 ]; then
-    echo "No arguments supplied. Run e.g. install-tessdata.sh eng deu. Aborting."
+    echo "No arguments supplied. Run e.g. 'install-tessdata.sh eng'. Aborting."
+    exit 1
+  fi
+  if [ $# -gt 1 ]; then
+    echo "Too many arguments supplied. Run e.g. 'install-tessdata.sh eng'. Aborting."
     exit 1
   fi
 }
 
-verify_tessdata_environment_variable() {
+verify_tessdata_prefix_set() {
   if [ -z "${TESSDATA_PREFIX}" ]; then
     echo "Environment variable TESSDATA_PREFIX is not set. Aborting."
     exit 2
   fi
 }
 
-install_tessdata_languages() {
-  echo "Installing $# languages into ${TESSDATA_PREFIX}..."
-  for language in "$@"; do
-    filename="${language}.traineddata"
-    wget -q "https://github.com/tesseract-ocr/tessdata/raw/3.04.00/${filename}"
-    mv "$filename" "$TESSDATA_PREFIX"
-    echo "Installed $filename"
-  done
+verify_tessdata_tag_set() {
+  if [ -z "${TESSDATA_TAG}" ]; then
+    echo "Environment variable TESSDATA_TAG is not set. Please set it to a valid github tag of tesseract-ocr/tessdata. Aborting."
+    exit 2
+  fi
 }
 
-verify_arguments_given "$@"
-verify_tessdata_environment_variable
-install_tessdata_languages "$@"
+install_tessdata_language() {
+  language=$1
+  echo "Loading tessdata language $language..."
+  filename="$language.traineddata"
+  wget --no-verbose -P "$TESSDATA_PREFIX" "https://github.com/tesseract-ocr/tessdata/raw/${TESSDATA_TAG}/${filename}"
+}
+
+verify_single_argument_given "$@"
+verify_tessdata_prefix_set
+verify_tessdata_tag_set
+install_tessdata_language "$1"
